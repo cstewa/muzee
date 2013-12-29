031138709ec3eeafe89b047a09bd63153d2bce42
Christina <christina.v.stewart@gmail.com>
2013-12-29 16:07:58 -0500

    first test commit

---
+++ muzee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muzee::Application.config.secret_token = '565c9201e45a2a386d40238a721509ed0bb415effee3fc63d40cc9d178e8932261e57159711326254bf456efe4f2c01b9aa02efe32592e869a543804ba533d45'
